dbd887a121a2cd16db7027be40168150325514c1
Azmi1180 <muhammadazmi4456@gmail.com>
2025-04-07 11:09:38 +0700

    Ubah Main.dart jadi ada dot.env dan menggunakan Async

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'dart:async'; 
+
 import 'onboarding_screen.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  try {
+      await dotenv.load(fileName: ".env");
+      print(".env file loaded successfully."); 
+  } catch (e) {
+      print("Error loading .env file: $e"); 
+  }
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp(      
       home: const OnboardingScreen(),
       debugShowCheckedModeBanner: false,
     );
